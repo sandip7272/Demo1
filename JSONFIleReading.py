9458bfef3b4db4652998162f1f54a8c56430ed45
sandip <deshmukhsandip2@gmail.com>
2019-01-18 14:43:24 +0530

    First Comment12

--- JSONFIleReading.py
+++ JSONFIleReading.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 jsondf = spark.read.json("/home/sandip/Downloads/dataJan-17-2019.json")
-
-
-# In[ ]:
 
 
 jsondf.printSchema()
 
-
-# In[ ]:
-
-
 jsondf.show()
 
-
-# In[ ]:
 
 
 

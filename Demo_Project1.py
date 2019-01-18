2806d96ce089ed82c3bc1c31055afa4d52e3b898
sandip <deshmukhsandip2@gmail.com>
2019-01-18 15:49:24 +0530

    First Comment123

--- Demo_Project1.py
+++ Demo_Project1.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local").appName("Demo1").getOrCreate()
 spark
-csvDF = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/usr/local/spark-2.4.0-bin-hadoop2.7/examples/src/main/resources/people.csv")
-
+csvDF = spark.read.format("csv").option("header","true").option("inferSchema","true").\
+    load("/usr/local/spark-2.4.0-bin-hadoop2.7/examples/src/main/resources/people.csv")
 csvDF.printSchema()

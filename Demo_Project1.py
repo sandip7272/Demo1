acc8ee738bf558bfe618e0af7984f7e6ec16594c
sandip <deshmukhsandip2@gmail.com>
2019-01-18 14:06:02 +0530

    First Comment

---
+++ Demo_Project1.py
@@ -0,0 +1,5 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local").appName("Demo1").getOrCreate()
+spark
+csvDF = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/usr/local/spark-2.4.0-bin-hadoop2.7/examples/src/main/resources/people.csv")
+csvDF.show()
